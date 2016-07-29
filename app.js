fabc27efba66bcafb703d6563ed1ab4aca277db4
waruna <waruna@duosoftware.com>
2016-07-29 16:23:39 +0530

    validations

--- app.js
+++ app.js
@@ -55,7 +55,37 @@ var setup_server = function (server) {
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    server.get('/facebook', function (req, res) {
+    server.get('/', function(req, res) {
+        console.log(req);
+        res.send('It works!');
+    });
+
+    server.get(['/facebook', '/instagram'], function(req, res) {
+        if (
+            req.param('hub.mode') == 'subscribe' &&
+            req.param('hub.verify_token') == 'token'
+        ) {
+            res.send(req.param('hub.challenge'));
+        } else {
+            res.send(400);
+        }
+    });
+
+    server.post('/facebook', function(req, res) {
+        console.log('Facebook request body:');
+        console.log(req.body);
+        // Process the Facebook updates here
+        res.send(200);
+    });
+
+    server.post('/instagram', function(req, res) {
+        console.log('Instagram request body:');
+        console.log(req.body);
+        // Process the Instagram updates here
+        res.send(200);
+    });
+
+    /*server.get('/facebook', function (req, res) {
         console.log("Facebook Callback........");
         console.log(req.params.hub.mode);
         console.log(req.params.hub.verify_token);
@@ -68,7 +98,7 @@ var setup_server = function (server) {
             console.log("Error");
             res.end(400);
         }
-    });
+    });*/
 
 
 };
