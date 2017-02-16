1a28e540380fd9aa276eea40c97f8236a00619b3
sukitha <sukitha@duosoftware.com>
2017-02-16 17:10:18 +0530

    fix

--- Services/FacebookClient.js
+++ Services/FacebookClient.js
@@ -844,7 +844,7 @@ var RealTimeComments = function (id, fbData) {
                 user.id = fbData.sender_id;
                 user.channel = 'facebook';
 
-                CreateEngagement("facebook-post", company, tenant, fbData.sender_name, to.name, "inbound", fbData.comment_id, fbData.message,user,fdData.sender_id,undefined, function (isSuccess, engagement) {
+                CreateEngagement("facebook-post", company, tenant, fbData.sender_name, to.name, "inbound", fbData.comment_id, fbData.message,user,fbData.sender_id,undefined, function (isSuccess, engagement) {
                     if (isSuccess) {
                         CreateComment('facebook-post', 'Comment', company, tenant, fbData.parent_id, undefined, engagement, function (done) {
                             if (!done) {
