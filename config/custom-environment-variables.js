fe0aeb90e094700d36c6d222df5c1a91aac5145c
achinthau <achintha@duosoftware.com>
2018-02-28 18:21:56 +0530

    update with FB VARS

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -111,7 +111,20 @@ module.exports = {
 
 		"facebookUrl" : "SYS_FACEBOOK_URL"
 
-    }
+    },
+	
+  "SocialConnector":
+  {
+    //"fb_client_id":"825442624259571",
+    //"fb_client_secret":"85eab7232db0c38abd7d04baa358382f",
+    //"owner_id":"124278164680278,124278164680845"
+
+
+    "fb_client_id":"SYS_FBCLIENT_ID",
+    "fb_client_secret":"SYS_FBCLIENT_KEY",
+    "owner_id":"SYS_FBOWNER_ID"
+
+  }
 };
 
 //NODE_CONFIG_DIR
